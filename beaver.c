5760336e8e5ee1fa87edea0b58d2b5927b479066
= <=>
2023-01-06 03:54:14 +0100

    init

---
+++ beaver.c
@@ -0,0 +1,26 @@
+#include "lib/beaver.h"
+
+#define FLAGS "-g -Wall -Werror -Og"
+
+module_t modules[] = {
+    { .name = "main", .src = "main.c" },
+};
+
+uint32_t modules_len = sizeof(modules) / sizeof(*modules);
+
+char* program[] = { "main", NULL };
+
+int main(int argc, char** argv)
+{
+    auto_update(argv);
+    if (argc == 1) {
+        compile(program, FLAGS);
+    } else if (strcmp(argv[1], "clean") == 0) {
+        rm("$(find build -type f)");
+        rm("out");
+    } else {
+        fprintf(stderr, "\033[31mError:\033[39m unknown option: '%s'\n", argv[1]);
+        exit(1);
+    }
+    return 0;
+}
